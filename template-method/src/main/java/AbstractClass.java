0ea437a246e7a1893403d3a882342cb0cf988296
James <gekylin@gmail.com>
2021-04-21 20:27:53 +0800

    添加单例UML类图

---
+++ template-method/src/main/java/AbstractClass.java
@@ -0,0 +1,10 @@
+/**
+ * @author James
+ * @date 2021/4/21
+ */
+public abstract class AbstractClass {
+    private void initialize(){}
+    private void specificMethod(){}
+    public abstract void abstractMethod1();
+    public abstract void abstractMethod2();
+}
